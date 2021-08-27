e51a45fd6cfc759c1fb9a0edc9c54475cac0a5a6
ZKitcher <49539587+ZKitcher@users.noreply.github.com>
2021-08-27 14:22:42 -0400

    Create HS Number Removal.user.js

---
+++ HS Number Removal.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         HS Number Removal
+// @namespace    https://www.hamiltonservices.com/
+// @version      0.0.1
+// @description  Removes the large quicksort numbers from HS.
+// @author       Z.Kitcher
+// @match        https://www.hamiltonservices.com/Web*
+// @match        https://www.hamiltonservices.com/*
+// @icon         https://www.hamiltonservices.com/Web/Content/favicon.svg
+// @include      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $('[name="quickValue"]').css('display','none')
+})();
