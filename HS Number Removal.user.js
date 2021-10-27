c9471a094658b9cfb4f118762532cec8848f0fb5
ZKitcher <49539587+ZKitcher@users.noreply.github.com>
2021-10-27 08:52:30 -0400

    Update HS Number Removal.user.js

--- HS Number Removal.user.js
+++ HS Number Removal.user.js
@@ -6,6 +6,8 @@
 // @author       Z.Kitcher
 // @match        https://www.hamiltonservices.com/web/LoyaltyAccount
 // @icon         https://www.hamiltonservices.com/web/Content/favicon.svg
+// @include      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @update       https://github.com/ZKitcher/RandomScripts/raw/main/HS%20Number%20Removal.user.js
 // ==/UserScript==
 
 (function() {
