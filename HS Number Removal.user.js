e127aacb930a73358e407303e8c4a932b6bcfbad
ZKitcher <49539587+ZKitcher@users.noreply.github.com>
2023-03-28 16:02:29 -0400

    Update HS Number Removal.user.js

--- HS Number Removal.user.js
+++ HS Number Removal.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         HS Number Removal
 // @namespace    https://www.hamiltonservices.com/
-// @version      0.0.2
+// @version      0.0.3
 // @description  Removes the large quicksort numbers from HS.
 // @author       Z.Kitcher
-// @match        https://www.hamiltonservices.com/web/LoyaltyAccount
+// @match        https://www.hamiltonservices.com/web/*
 // @icon         https://www.hamiltonservices.com/web/Content/favicon.svg
 // @include      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @update       https://github.com/ZKitcher/RandomScripts/raw/main/HS%20Number%20Removal.user.js
@@ -12,5 +12,5 @@
 
 (function() {
     'use strict';
-    $('[name="quickValue"], #activeUsers, #creditHold, #endBilling, #totalUsers').css('display','none')
+    $('body').on('DOMSubtreeModified', () => $('[name="quickValue"]').css('display','none'))
 })();
